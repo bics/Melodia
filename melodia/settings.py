6e1e5e49948db16d1519eaba021846513facb76b
bics <bicsi94@gmail.com>
2026-01-30 19:12:35 +0000

    ME-Styling base

--- melodia/settings.py
+++ melodia/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Allauth authentication block taken from offiial documentation
