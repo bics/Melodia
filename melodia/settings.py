8652cc2ad0d03c7be8f78676f5d8c71927419f71
Otthon <bicsi94@gmail.com>
2026-02-04 13:50:38 +0100

    ME-Adding and styling table

--- melodia/settings.py
+++ melodia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-9agbtx0+#i01au+&j9%bm#ks#fyh_nkw!-etr(fs5o@xo7eced
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",]
 
 
 # Application definition
