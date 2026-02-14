5a3ee2992c242642e53bc2f343e1929897846a74
bics <bicsi94@gmail.com>
2026-02-14 17:34:46 +0000

    ME-Creating album class

--- album/models.py
+++ album/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    released = models.DateField()
+    length = models.IntegerField()
+    rating = models.FloatField(blank=True, null=True)
+
+    # TODO implement relation/extra fields
+    # artist
+    # tracks
+    # manager
