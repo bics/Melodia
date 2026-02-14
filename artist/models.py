98000362accef52528d77c488aa6058e1d724380
bics <bicsi94@gmail.com>
2026-02-14 15:11:24 +0000

    ME-Initial artist model

--- artist/models.py
+++ artist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    bornOn = models.DateField()
+    socials = models.TextField(blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+
+    # TODO implement relation/extra fields
+    # albums
+    # tracks
+    # manager
+    
