5994d47973fbe73a4f9c16a330dca549500e58b6
bics <bicsi94@gmail.com>
2026-02-14 18:16:40 +0000

    ME-Adding custom user as auth user

--- melodia/settings.py
+++ melodia/settings.py
@@ -88,6 +88,8 @@ WSGI_APPLICATION = 'melodia.wsgi.application'
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
+# Account modifications
+AUTH_USER_MODEL = 'members.MelodiaUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- members/models.py
+++ members/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class MelodiaUser(AbstractUser):
+    isManager = models.BooleanField(default=False)
