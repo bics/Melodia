c2aacc8bd92fcfd9eac085b4498c9b4f2eda3eb3
bics <bicsi94@gmail.com>
2026-02-14 15:02:06 +0000

    ME-Creating artist project

--- melodia/urls.py
+++ melodia/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('accounts/', include('allauth.urls')),
-    path('', include('landingpage.urls'))
+    path('', include('landingpage.urls')),
+    path('artist/', include('artist.urls')),
 ]
