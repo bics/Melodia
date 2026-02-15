f0c58dd2ae661db675d9c38ec13e8b908c0076df
bics <bicsi94@gmail.com>
2026-02-15 15:38:25 +0000

    ME-Creating account sites

--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.members, name='members'),
+    path('favourites/', views.favourites, name='favourites'),
+    path('account/', views.account, name='account'),
+
 ]
--- members/views.py
+++ members/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def members(request):
     return render(request, 'artist.html')
+
+
+def favourites(request):
+    return render(request, 'favourites.html')
+
+
+def account(request):
+    return render(request, 'account.html')
