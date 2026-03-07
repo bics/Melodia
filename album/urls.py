d7d10e91ac7dcf420c50cdad578972db57a025c7
bics <bicsi94@gmail.com>
2026-03-07 16:43:32 +0000

    ME-Adding edit to albums

--- album/urls.py
+++ album/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.album, name='album'),
+    path('<str:name>/<int:pk>/<int:artistPK>', views.album, name='album'),
 ]
--- album/views.py
+++ album/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from artist.models import Artist
+from .models import Album
+from artist.forms import AlbumCreationForm
 
 # Create your views here.
-def album(request):
-    return render(request, 'album.html')
+def album(request, name, pk, artistPK):
+    album = Album.objects.get(pk=pk)
+    artist = Artist.objects.get(pk=artistPK)
+
+    if request.method == "POST":
+        form = AlbumCreationForm(request.POST, request.FILES, instance=album)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Album edited successfully for {artist.name}")
+            return redirect("artist", name=artist.name, pk=artist.pk)
+        else:
+            messages.success(request, ("There were some errors with some fields"))
+    else:  
+        form = AlbumCreationForm(instance=album)
+
+
+    return render(request, 'album.html', {'form' : form, 'album': album, 'artist': artist})
