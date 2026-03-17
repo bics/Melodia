8d81102993f48a9b2b3259e92181eb01e6a8ea8c
bics <bicsi94@gmail.com>
2026-03-17 21:29:49 +0000

    ME-Starting playtrack function

--- static/assets/album/js/artist_audio_player.js
+++ static/assets/album/js/artist_audio_player.js
@@ -1,14 +1,18 @@
 
 const mp3AudioPlayerSource = document.getElementById("mp3-audio-source")
+const currentlyPlayingHeader = document.getElementById("currently-playing-header");
 const play_buttons = document.querySelectorAll('.audio-control-button');
 
-//Function generated using ChatGPT
-play_buttons.forEach(button => {
-  button.addEventListener('click', function (event) {
-    const el = event.currentTarget; // or just "this"
-
-    console.log(el.id);
-    console.log(el.dataset.track);
-    console.log(el.dataset.trackPosition);
-  });
+//Function partially generated using ChatGPT
+play_buttons.forEach(button =>
+{
+    button.addEventListener('click', () => playTrack(event)
+    )
 });
+
+function playTrack(event)
+{
+    const el = event.currentTarget;
+
+    currentlyPlayingHeader.innerText = "Currently playing: " + el.dataset.track;
+}
