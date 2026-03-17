8f751f387df15c450cf2b85ad32c0d45ad42ed73
bics <bicsi94@gmail.com>
2026-03-17 21:44:08 +0000

    ME-Displaying next track

--- static/assets/album/js/artist_audio_player.js
+++ static/assets/album/js/artist_audio_player.js
@@ -1,18 +1,37 @@
 
 const mp3AudioPlayerSource = document.getElementById("mp3-audio-source")
 const currentlyPlayingHeader = document.getElementById("currently-playing-header");
+const nextPlayingHeader = document.getElementById("next-playing-header");
 const play_buttons = document.querySelectorAll('.audio-control-button');
 
 //Function partially generated using ChatGPT
 play_buttons.forEach(button =>
 {
-    button.addEventListener('click', () => playTrack(event)
-    )
+    button.addEventListener('click', playTrack)
 });
+
+//<img class="audio-control-button" 
+// id="audio-control-button-{{album.id}}-{{forloop.counter0}}" 
+// src="{% static 'assets/images/play_button_small.png' %}" 
+// alt="Play track button"
+// data-track="{{ track.name }}" 
+// data-track-position="{{ track.position }}">
 
 function playTrack(event)
 {
     const el = event.currentTarget;
 
     currentlyPlayingHeader.innerText = "Currently playing: " + el.dataset.track;
+    nextPlayingHeader.innerHTML = "Next: " + getNextTrack(el.id);
+}
+
+function getNextTrack(currentIDString)
+{
+    const interData = currentIDString.split('-');
+    const currentAlbumIndex = parseInt(interData[3]);
+    const currentTrackIndex = parseInt(interData[4]);
+
+    const nextTrackButton = document.getElementById("audio-control-button-" + currentAlbumIndex + "-" + (currentTrackIndex + 1));
+
+    return nextTrackButton.dataset.track;
 }
