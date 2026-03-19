7622ecb186ff9ccfc388e74720d0014be9fffe7b
bics <bicsi94@gmail.com>
2026-03-19 16:15:22 +0000

    ME-Landing page rendomly displays 15 artists

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from artist.models import Artist
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # Random object retrieval generated using ChatGPT
+    artists = Artist.objects.order_by('?')[:15]
+    return render(request, 'index.html', {"artists": artists})
