999ce890a6dd811d67822dfb50d9a928d127c372
bics <bicsi94@gmail.com>
2026-03-19 17:08:04 +0000

    ME-Adding  static root

--- melodia/settings.py
+++ melodia/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Allauth authentication block taken from offiial documentation
 
