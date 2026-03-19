45a5191fcfb3433d40f7e06ceb67b71728b3d178
bics <bicsi94@gmail.com>
2026-03-19 16:22:13 +0000

    ME-Sorting artists based on ratings

--- landingpage/views.py
+++ landingpage/views.py
@@ -4,5 +4,9 @@ from artist.models import Artist
 # Create your views here.
 def index(request):
     # Random object retrieval generated using ChatGPT
-    artists = Artist.objects.order_by('?')[:15]
+    artists = sorted(
+        Artist.objects.order_by('?')[:15],
+        key=lambda x: x.rating or 0,
+        reverse=True
+    )
     return render(request, 'index.html', {"artists": artists})
