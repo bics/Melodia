d84b18df6712e3719dce10e14521dc0d8c90c763
bics <bicsi94@gmail.com>
2026-03-29 21:44:09 +0100

    ME-Rating works on landingpage

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from artist.models import Artist
 from album.models import Track, Album
 import random
-from django.db.models import Avg, Value
+from django.db.models import Avg, Value, Count, Case, When, FloatField
 from django.db.models.functions import Replace
 
 # Create your views here.
 def index(request):
-    # Random object retrieval and sorting generated using ChatGPT
+    # Random object retrieval, sorting and rating generated using ChatGPT
     artists = list(
         Artist.objects.annotate(
         avg_rating=Avg("tracks__rating__ratingValue")
@@ -16,9 +16,25 @@ def index(request):
     random.shuffle(artists)
     artists = sorted(artists[:15], key=lambda x: x.avg_rating or 0, reverse=True)
 
-    all_tracks = list(Track.objects.filter(artist__in=artists))
+    all_tracks = list(
+        Track.objects.filter(artist__in=artists).annotate(
+            rating_count=Count("rating"),
+            avg_rating=Case(
+                When(rating_count__gte=3, then=Avg("rating__ratingValue")),
+                default=None,
+                output_field=FloatField()
+            )
+        )
+    )
     random.shuffle(all_tracks)
     random_tracks = all_tracks[:10]
+
+    for track in random_tracks:
+        if track.avg_rating:
+            filled = int(track.avg_rating)
+            track.stars = [True] * filled + [False] * (5 - filled)
+        else:
+            track.stars = [False] * 5
 
 
     return render(request, 'index.html', {"artists": artists, "random_tracks": random_tracks})
