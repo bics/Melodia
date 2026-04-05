58805e9e9a70086f24a86e47c731bf8fc17ad263
bics <bicsi94@gmail.com>
2026-04-05 18:21:47 +0100

    ME-Updating and linking donation page

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payment, name='payment'),
+    path('<str:name>/<int:pk>/donate', views.donate, name='donate'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from artist.models import Artist
 
 # Create your views here.
-def payment(request):
-    return render(request, "stripe_payment.html")
+def donate(request, name, pk):
+    artist = Artist.objects.get(pk=pk)
+    return render(request, "stripe_payment.html", { "artist" : artist })
