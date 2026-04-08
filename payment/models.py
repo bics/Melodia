523c0b9647b32046abbac72ec4240b23f3d64ff5
bics <bicsi94@gmail.com>
2026-04-08 18:24:25 +0100

    ME-Creating payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from artist.models import Artist
 
 # Create your models here.
+class Donation(models.Model):
+    amount = models.IntegerField()
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True)
+    artist_name = models.CharField()
+    stripe_session_id = models.CharField()
+    is_paid = models.BooleanField(default=False)
+    time = models.DateTimeField(auto_now_add=True)
+
+
