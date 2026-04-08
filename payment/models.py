56bb6b6b6a258c0608ad6438caf318839f934174
bics <bicsi94@gmail.com>
2026-04-08 18:35:20 +0100

    ME-Couple more fields, req fields filled

--- payment/models.py
+++ payment/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from artist.models import Artist
+from members.models import MelodiaUser
 
 # Create your models here.
 class Donation(models.Model):
     amount = models.IntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True)
-    artist_name = models.CharField()
-    stripe_session_id = models.CharField()
+    artist_name = models.CharField(max_length=255)
+
+    manager_name = models.CharField(max_length=255)
+    manager_stripe_id = models.CharField(max_length=255)
+
+    donator = models.ForeignKey(MelodiaUser, on_delete=models.SET_NULL, null=True, blank=True)
+    donator_email = models.CharField(max_length=255)
+
+    stripe_session_id = models.CharField(max_length=255, unique=True)
     is_paid = models.BooleanField(default=False)
     time = models.DateTimeField(auto_now_add=True)
 
